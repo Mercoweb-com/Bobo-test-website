0c0772cfad9bde5a60e914686e3528257be06f44
Mercoweb-com <mercyonwe22@gmail.com>
2024-12-14 11:30:15 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,18 @@
+const sheetID='1C-O7klKaLnSN3pJBV2SAHwXhAAXrKQXV3z3btLRsH48';
+const base='https://docs.google.com/spreadsheets/d/${sheetID}/gvz/tq?';
+const sheetname='users';
+const query=encodeURIComponent('select *');
+const url=' ${base}&sheet=${Sheet Name}&tq=${query}';
+const data =[];
+document.addEventListener('DOMContentLoaded',init);
+
+fuction init(){
+    console.log('ready');
+    fetch (url)
+}
+.then(res=> res.text())
+.then(rep=> {
+    // console.log(rep);
+    const jsO= JSON.parse(rep.subtrs(47).slice(0,-2));
+    console.log(jsO);
+})
