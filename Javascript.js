2fdaf63f78b095203a9aec792fe1e06bea9dc9ca
Mercoweb-com <mercyonwe22@gmail.com>
2024-12-21 16:39:51 +0100

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -5,19 +5,8 @@ const form = document.forms['contact-form']
 form.addEventListener('next', e=> { e.preventDefult()
 fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => alert("Thank you! your form is submitted successfully.")
-const sheetname='users';
-const query=encodeURIComponent('select *');
-const url=' ${base}&sheet=${Sheet Name}&tq=${query}';
-const data =[];
-document.addEventListener('DOMContentLoaded',init);
 
-fuction init(){
-    console.log('ready');
-    fetch (url)
-}
-.then(res=> res.text())
-.then(rep=> {
-    // console.log(rep);
+        // console.log(rep);
     const jsO= JSON.parse(rep.subtrs(47).slice(0,-2));
     console.log(jsO);
 })
