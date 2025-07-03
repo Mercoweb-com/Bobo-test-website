9a4539d700aae450a7c96e090852e9bbba26a60b
Mercoweb-com <mercyonwe22@gmail.com>
2025-07-03 16:44:52 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-scriptURconstL = 'https://script.google.com/macros/s/AKfycbzv_b0sbP0IXjHSytjhpr0240UHgmNiY0WA4cvQHMqgPTq9SpmZzINFfsP3ebq8Sp_IxA/exec'
-
-const form = document.form['contact-form']
-
-form.addEventListener('submit', e => {
-    e.preventDefult()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-.then(Response => alert("Thank you! your form is submitted suceessfully."))
-.then(() => { window.location.reload(); })
-.catch(error => console.error('Error!', error.message));
-})
+<script>
+  document.getElementById("navToggle").addEventListener("click", function () {
+    document.getElementById("navLinks").classList.toggle("show");
+  });
+</script>
